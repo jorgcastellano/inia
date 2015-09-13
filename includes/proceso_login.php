6c8635253bcc14db3a937fe377e369e76e675969
Jorge Castellano <jorgecm14@gmail.com>
2015-09-13 13:30:06 -0430

    resuelto la profundidad de los menus e incorporacion de codigo de inicio de sesion segura

--- includes/proceso_login.php
+++ includes/proceso_login.php
@@ -40,4 +40,28 @@
 		//En caso de que el formulario se encuentren vacios
 		header("location: ../../0/home/index");
 	endif;
+
+/*
+ * Procesamiento de inicio de sesion *
+*/
+/*
+include_once 'functions.php';
+ 
+sec_session_start(); // Nuestra manera personalizada segura de iniciar sesión PHP.
+ 
+if (isset($_POST['email'], $_POST['p'])) {
+    $email = $_POST['email'];
+    $password = $_POST['p']; // La contraseña con hash
+ 
+    if (login($correo, $password, $mysqli) == true) {
+        // Inicio de sesión exitosa
+        header('Location: ../protected_page.php');
+    } else {
+        // Inicio de sesión exitosa
+        header('Location: ../index.php?error=1');
+    }
+} else {
+    // Las variables POST correctas no se enviaron a esta página.
+    echo 'Solicitud no válida';
+}*/
 ?>
