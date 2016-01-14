52e2b2413414997b0419d3cbf977a4d53719f0d8
yitzon <jitzon.jose@gmail.com>
2016-01-14 16:43:14 -0430

    union de aceptacion de usuario con gestacion de usuario

--- 0/home/gestion_usuario.php
+++ 0/home/gestion_usuario.php
@@ -33,7 +33,7 @@
                         endif;
                     endfor;
                 endif;
-                $reg = $objinicio->consultar_miembro_on($mysqli);
+                $reg = $objinicio->consultar_miembro($mysqli);
 
                 echo "<form action='gestion_usuario' method='POST'>
                 <table class='usuario'>
@@ -41,11 +41,46 @@
                         <td><i class='fa fa-chevron-circle-right'></i> Cédula</td>
                         <td>Usuario</td>
                         <td>Email</td>
+                        <td>Aceptación</td>
                         <td>Privilegios</td>
                         <td><i class='fa fa-trash-o'></i></td>
                     </tr>";
 
                 while ($resultado = $reg->fetch_array()) :
+
+                    //Verificacion de los seleccionados para ser almacenados
+                    if (isset($guardar)) :
+                        for ($x=0; $x < $temp = count($cod); $x++)
+                            if ($resultado[0] == $cod[$x]) :
+                                if ($resultado[9] == "On")
+                                    $x=$temp;
+                                else
+                                    $on = $resultado[0];
+                                    $x=$temp;
+                            elseif ($x == ($temp-1)) :
+                                if ($resultado[9] == "On")
+                                    $off = $resultado[0];
+                            endif;
+
+                        if (isset($on)) :
+                            $objinicio->modificar_miembros_estatus($mysqli, "On", $on);
+                            $resultado[9] = "On";
+                        elseif (isset($off)) :
+                            $objinicio->modificar_miembros_estatus($mysqli, "Off", $off);
+                            $resultado[9] = "Off";
+                        endif;
+                        unset($off, $on);
+                    endif;
+                    if (isset($guardar) AND !isset($cod)) {
+                        $objinicio->modificar_miembros_estatus_all($mysqli, "Off");
+                        $resultado[9] = "Off";
+                    }
+                    if ($resultado[9] == "On") :
+                        $checked = "checked";
+                    elseif ($resultado[9] == "Off") :
+                        $checked = "";
+                    endif;
+
                     if ($resultado[10] == 1) :
                         $uno = "selected";
                     elseif ($resultado[10] == 2) :
@@ -54,11 +89,14 @@
                         $tres = "selected";
                     endif;
                     echo "<tr>
-                        <td>$resultado[1]
-                        <input type='hidden' name='codigos[]' value='$resultado[0]' /></td>
-                        <td>$resultado[2] $resultado[3]</td>
-                        <td>$resultado[4]</td>
-                        <td><select name='privilegios[]'>
+                            <td>$resultado[1]
+                            <input type='hidden' name='codigos[]' value='$resultado[0]' /></td>
+                            <td>$resultado[2] $resultado[3]</td>
+                            <td>$resultado[4]</td>
+                            <td>
+                                <input type='checkbox' name='cod[]' value='$resultado[0]' title='click para seleccionar los usuarios que desea aceptar' $checked/>
+                            </td>
+                            <td><select name='privilegios[]'>
                             <option value=''        >-seleccione-</option>
                             <option value='1' $uno  >Gerente</option>
                             <option value='2' $dos  >Especialista</option>
@@ -72,7 +110,7 @@
             ?>
             <div align="center">
                 <button type="button" name="regresar" onclick=location="inicio" class="boton"><i class="fa fa-home"></i> Regresar a inicio</button>
-                <button type="submit" name="guardar" class="boton"><i class="fa fa-check"></i> Guardar cambios</button>
+                <button type="submit" name="guardar" value="guardar" class="boton"><i class="fa fa-check"></i> Guardar cambios</button>
             </div>
             </form>
             <?php include '../../layouts/layout_p.php' ?>
