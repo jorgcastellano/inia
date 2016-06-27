ffce23c32dbc215d6f13a7f197edf34482ca062b
Jorge Castellano <jorgecm14@gmail.com>
2016-06-27 00:24:46 -0400

    Página de bloqueo

--- 0/home/block.php
+++ 0/home/block.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
-
-</body>
-</html>
+	<head>
+		<title>Usuario bloqueado</title>
+		<?php include_once '../../layouts/head.php' ?>
+	</head>
+	<body>
+	 	<section class="bloque">
+            <div>
+            	<?php include_once '../../layouts/cabecera-body.php' ?>
+	            <hgroup>
+	                <h1>¡Hay un error! disculpe</h1>
+	            </hgroup>
+            </div>
+			<p>Al parecer su usuario está bloqueado por intentos máximos fallidos para el ingreso del sistema</p>
+			<p>Por favor, contacte al gerente para que te elimine el bloqueo y puedas disfrutar de los servicios que ofrecemos</p>
+            <div align="center">
+            	<button type='button' onclick=location='../../0/home/cerrar_sesion' class="boton"><i class="fa fa-home"></i> Página principal</button>
+ 			</div>
+            <?php include_once '../../layouts/layout_p.php' ?>
+        </section>
+	</body>
+</html>../
