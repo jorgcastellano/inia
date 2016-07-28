cc44ac8be495e7fa117ce6565307792cc5fcbc90
Jorge Castellano <jorgecm14@gmail.com>
2016-07-27 20:41:53 -0400

    Nuevos cambios e insercion de algoritmos basicos para email

--- includes/config_bd.php
+++ includes/config_bd.php
@@ -1,11 +1,11 @@
 <?php 
 	
 
-	define("HOST", "inia.jcm.co.ve");
+	define("HOST", "31.220.104.130");
  	//define("USER", "sec_user");
-  	define("USER", "gproyecto");
+  	define("USER", "u817028193_uptm");
   	define("PASSWORD", "123456");
   	//define("PASSWORD", "AHxySPDT7U8mNTdE");
-  	define("DATABASE", "proyecto3");
+  	define("DATABASE", "u817028193_uptm");
 
 ?>
---
+++ sistaxis_email.php
@@ -0,0 +1,38 @@
+<?php 
+$destinatario = "jorgcastellano@gmail.com"; 
+$asunto = "Este mensaje es de prueba"; 
+$cuerpo = ' 
+<html> 
+<head> 
+   <title>Prueba de correo</title> 
+</head> 
+<body> 
+<h1>Hola amigos!</h1> 
+<p> 
+<b>Bienvenidos a mi correo electrónico de prueba</b>. Estoy encantado de tener tantos lectores. Este cuerpo del mensaje es del artículo de envío de mails por PHP. Habría que cambiarlo para poner tu propio cuerpo. Por cierto, cambia también las cabeceras del mensaje. 
+</p> 
+</body> 
+</html> 
+'; 
+
+//para el envío en formato HTML 
+$headers = "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+
+//dirección del remitente 
+$headers .= "From: Jorge Agustin Castellano <jorge@dev.web.ve>\r\n"; 
+
+//dirección de respuesta, si queremos que sea distinta que la del remitente 
+//$headers .= "Reply-To: mariano@desarrolloweb.com\r\n"; 
+
+//ruta del mensaje desde origen a destino 
+//$headers .= "Return-path: holahola@desarrolloweb.com\r\n"; 
+
+//direcciones que recibián copia 
+//$headers .= "Cc: maria@desarrolloweb.com\r\n"; 
+
+//direcciones que recibirán copia oculta 
+//$headers .= "Bcc: pepe@pepe.com,juan@juan.com\r\n"; 
+
+mail($destinatario,$asunto,$cuerpo,$headers); 
+?>
