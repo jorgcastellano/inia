4de458e8015636a9869868360a3c2af8db5ee541
Jorge Castellano <jorgecm14@gmail.com>
2016-09-30 10:58:30 -0400

    Fix en fincas y mejoras en la modificacion de datos de los clientes

--- 0/cliente/index.php
+++ 0/cliente/index.php
@@ -19,14 +19,14 @@
 		<?php
 			if (isset($_GET['var1']))
 				$var1=$_GET["var1"];
-	        
+
 	        if(isset($_POST['Modificar'])) :
 	    		extract($_POST);
 	            $Ced_cliente=$Modificar;
 	            require_once '../../system/class.php';
 				$client = new cliente();
 				$reg = $client->consultar_cliente($mysqli,$Ced_cliente);
-				if ($_SESSION['privilegios'] == 1) : 
+				if ($_SESSION['privilegios'] == 1) :
 		            $fin = new finca();
 		            $reg2=$fin->consultar_finca_all($mysqli,$Ced_cliente);
 	            endif;
@@ -34,8 +34,8 @@
 		?>
 
 			<form class="contact_form" method="post" action="resultados">
-				
-				<label for="Ced_cliente">Cédula</label>
+
+				<br><label for="Ced_cliente">Cédula</label>
                 <input required type="num" name="Ced_cliente" id="Ced_cliente" value="<?php echo $reg[1].$var1; ?>" title="Introduzca la cédula " maxlength="8" placeholder="" pattern="\d{6,}" />
                 <span class="form_hint">Debe contener solo caracteres númericos"</span><br />
 				<label for="Nom_cliente">Nombres</label>
@@ -55,18 +55,25 @@
 					<br />
                 <?php
                 if ($_SESSION['privilegios'] == 1) :
-                	while($reg3 = $reg2->fetch_array()) :
-                		include 'lib_finca.php';
-                		echo "<input type='hidden' name='cod_finca[]' value='$reg3[0]'>";
-                	endwhile;
+                  $j = 0;
+                  if (!empty($reg2)) {
+                    while($reg3 = $reg2->fetch_array()) :
+                      $j++;
+                      echo "<h3>Finca $j</h3><hr>";
+                  		include 'lib_finca.php';
+                  		echo "<input type='hidden' name='cod_finca[]' value='$reg3[0]'>";
+                  	endwhile;
+                  }
+                	else{
+                    echo "<h3>Datos de la finca</h3><hr>";
+                    include 'lib_finca.php';
+                  }
                 endif;
 
                 //<label for="persona" title="Seleccione si es una persona juridica">Persona juridica</label>
 				//<input type="checkbox" name="Nat_jur[]" value="1" title="Seleccione si es una persona juridica"/>
-					
+
                 ?>
-
-
                 <div class="grupobotones">
 					<button type="reset" class="boton" name="reset" title="Click aquí para quitar todos los datos que fueron llenados en el formulario"><i class="fa fa-eraser" ></i> Limpiar formulario</button>
 					<?php if(isset($Modificar)): ?><button type="submit" formaction="resultados" name="Actualizar" value="<?php echo $reg[0]?>" class="boton" ><i class="fa fa-check"></i> Guardar cambios</button>
