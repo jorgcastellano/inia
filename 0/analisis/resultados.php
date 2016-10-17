8839f07d0c62774147bf8a82ea14308c3d498cb3
canagua <aromyosman@gmail.com>
2016-10-17 18:00:45 -0430

    mensajes

--- 0/analisis/resultados.php
+++ 0/analisis/resultados.php
@@ -26,7 +26,7 @@
                 require_once '../../system/class.php';
                 $ana = new analisis();
                 $ana->modificar_analisis($mysqli,$Cod_ana,$Nom_ana,$Precio_ana,$Tipo);
-                if($mysqli->affected_rows>0){ echo "<span class='notify'><i class='fa fa-check-square'></i>El nuevo analisis se ha modificar con exito</span> ";} 
+                if($mysqli->affected_rows>0){ echo "<span class='notify'><i class='fa fa-check-square'></i>El nuevo analisis se ha modificado con exito</span> ";} 
                     else { echo "<span class='notify_f'><i class='fa fa-times'></i>No se ha podido modificar el nuevo analisis</span> ";}
                 $reg=$ana->consultar_analisis($mysqli,$Cod_ana);
                 endif;
