2c5f5e7e15da884f8a83458cdc221b1ed921aab6
Ines Benitez <ines.benitez2@gmail.com>
2016-11-09 00:33:28 -0400

    plantillas para listados de muestras y modificacion de clases especialista con muestra

---
+++ 0/laboratorio/lib_asig_especialista.php
@@ -0,0 +1,37 @@
+<?php
+
+        $estatus="";
+        $objmuestra = new muestra();
+        $reg = $objmuestra -> consultar_muestras($mysqli,$estatus);
+        $objespecialista = new especialista();
+        $reg2 = $objespecialista -> consulta_completo($mysqli);
+
+      echo "<form action='' method='POST' onsubmit=''>
+              <table class='usuario'>
+                <tr>
+                    <td><i class='fa fa-chevron-circle-right'></i> Código </td>
+                    <td>Tipo</td>
+                    <td>Fecha inicio</td>
+                    <td>Fecha final </td>
+                    <td>Asignar</td>
+                </tr>";
+
+    while ($res = $reg -> fetch_array()) :
+
+        echo "    <tr>
+                      <td>$res[]</td>
+                      <td>$res[]</td>
+                      <td>$res[]</td>
+                      <td>$res[]</td>
+                      <td>
+                        <select>
+                            <option value='' ></option>"
+                          while($res2 = $reg2 -> fetch_array()):
+        echo "              <option value=''></option>";
+                          endwhile;
+        echo "          </select>
+                      </td>
+                  </tr>";
+    endwhile;
+
+ ?>
---
+++ 0/laboratorio/lib_procesando.php
@@ -0,0 +1,28 @@
+<?php
+
+        $estatus="";
+        $objmuestra = new muestra();
+        $reg = $objmuestra -> consultar_muestras($mysqli,$estatus);
+
+      echo "<form action='' method='POST' onsubmit=''>
+              <table class='usuario'>
+                <tr>
+                    <td><i class='fa fa-chevron-circle-right'></i> Código </td>
+                    <td>Tipo</td>
+                    <td>Fecha inicio</td>
+                    <td>Fecha final </td>
+                    <td>Asignar</td>
+                </tr>";
+
+    while ($res = $reg->fetch_array()) :
+
+        echo "    <tr>
+                      <td>$res[]</td>
+                      <td>$res[]</td>
+                      <td>$res[]</td>
+                      <td>$res[]</td>
+                      <td></td>
+                  </tr>";
+    endwhile;
+
+ ?>
